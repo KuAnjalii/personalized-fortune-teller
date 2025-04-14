e9b75a82c7324b8df1ba991ca8fd80d0a8d5a2f8
KuAnjalii <anjaliair111@gmail.com>
2025-04-14 11:02:37 +0530

    Initial commit - basic version v1.0

---
+++ fortune.py
@@ -0,0 +1,15 @@
+
+print("🔮 Welcome to Kumari Anjali's Fortune Teller (21JE0495) ")  
+
+mood = input("How are you feeling today? (happy/sad/neutral): ").strip().lower()
+
+if mood == "happy":
+    print(" Your fortune: Great things await you, Kumari Anjali! Keep smiling.")
+elif mood == "sad":
+    print(" Your fortune: Tough days don’t last. Better days are coming.")
+elif mood == "neutral":
+    print(" Your fortune: A surprise will brighten your day soon.")
+else:
+    print(" Sorry, I don't have a fortune for that mood yet. ")
+
+# https://github.com/KuAnjalii/personalized-fortune-teller
